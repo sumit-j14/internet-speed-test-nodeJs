9a05db3587bc3968b54b7ec2647d754e3cc05087
Sumit <sumit.jerry1409@gmail.com>
2022-03-27 18:58:56 +0530

    first commit after test

---
+++ Readme.md
@@ -0,0 +1,4 @@
+This is a simple web app for calculating the approximate internet speed.
+uses an npm module called network-speed
+to run type in terminal npm install 
+then type node app.js
---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//npm speed test module
+const NetworkSpeed = require('network-speed');
+const testNetworkSpeed = new NetworkSpeed();
+
+// this is the function calls
+//prints download and upload speed in mbps in console
+getNetworkDownloadSpeed();
+
+async function getNetworkDownloadSpeed() {
+  const baseUrl = 'https://eu.httpbin.org/stream-bytes/500000';
+  const fileSizeInBytes = 500000;
+  const speed = await testNetworkSpeed.checkDownloadSpeed(baseUrl, fileSizeInBytes);
+  console.log("download speed is "+speed.mbps);
+
+}
+
+getNetworkUploadSpeed();
+
+async function getNetworkUploadSpeed() {
+  const options = {
+    hostname: 'www.google.com',
+    port: 80,
+    path: '/catchers/544b09b4599c1d0200000289',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  const fileSizeInBytes = 2000000
+  const speed = await testNetworkSpeed.checkUploadSpeed(options, fileSizeInBytes);
+  console.log("upload speed is "+speed.mbps);
+}
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
